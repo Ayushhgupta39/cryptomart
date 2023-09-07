892d084b7fad3bcdfa5d3ab2240987e70ff853f8
Ayush Gupta <ayushgupta3902@gmail.com>
2023-09-07 23:05:31 +0530

    Complete page layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { Layout, Space } from "antd";
+import { Route, Link, Routes } from "react-router-dom";
+import {
+  Navbar,
+  Homepage,
+  Exchanges,
+  Market,
+  CryptoDetails,
+  News,
+} from "./components";
+import "./App.css";
+import Typography from "antd/es/typography/Typography";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="main">
+        <Layout>
+          <div className="routes">
+            <Routes>
+              <Route exact path="/" element={<Homepage />}></Route>
+              <Route exact path="/exchanges" element={<Exchanges />}></Route>
+              <Route
+                exact
+                path="/cryptocurrencies"
+                element={<Market />}
+              ></Route>
+              <Route
+                exact
+                path="/crypto/:coinId"
+                element={<CryptoDetails />}
+              ></Route>
+              <Route exact path="/news" element={<News />}></Route>
+            </Routes>
+          </div>
+        </Layout>
+        <div className="footer">
+          <Typography.Title
+            level={5}
+            style={{ color: "white", textAlign: "center" }}
+          >
+            CryptoMart <br />
+            All rights reserved
+          </Typography.Title>
+          <Space>
+            <Link to={"/"}>Home</Link>
+            <Link to={"/exchanges"}>Exchanges</Link>
+            <Link to={"/news"}>News</Link>
+          </Space>
+        </div>
+      </div>
     </div>
   );
 }
