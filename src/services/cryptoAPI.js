c354fba607d3768b981f61b43fa90cd785553164
Ayush Gupta <ayushgupta3902@gmail.com>
2023-09-09 13:45:58 +0530

    Complete market section

---
+++ src/services/cryptoAPI.js
@@ -0,0 +1,38 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const cryptoApiHeaders = {
+  "X-RapidAPI-Key": "7f69b9a3e4msh9e8525f2142b8b0p1b0668jsndb030b4afaac",
+  "X-RapidAPI-Host": "coinranking1.p.rapidapi.com",
+};
+
+const baseUrl = "https://coinranking1.p.rapidapi.com/";
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
+
+export const cryptoApi = createApi({
+  reducerPath: "cryptoApi",
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptos: builder.query({
+      query: (count) => createRequest(`/coins?limit=${count}`),
+    }),
+  }),
+});
+
+export const { useGetCryptosQuery } = cryptoApi;
+
+// const options = {
+//     method: 'GET',
+//     url: 'https://coinranking1.p.rapidapi.com/exchanges',
+//     params: {
+//       referenceCurrencyUuid: 'yhjMzLPhuIDl',
+//       limit: '50',
+//       offset: '0',
+//       orderBy: '24hVolume',
+//       orderDirection: 'desc'
+//     },
+// headers: {
+//     'X-RapidAPI-Key': '7f69b9a3e4msh9e8525f2142b8b0p1b0668jsndb030b4afaac',
+//     'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
+//   }
+// };
